dbc01e8e992110c7a318a51a7bf9316644dee89c
PC13-IFLAB <PC13-IFLAB>
2023-03-20 08:19:30 +0700

    menambahkan class Penjumlahan dengan method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+ class Penjumlahan
+{
+    public int JumlahTigaAngka<T> (T x, T y, T z)
+    {
+        
+        return (dynamic) x + (dynamic)y + (dynamic)z;
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Penjumlahan penjumlahan= new Penjumlahan();
+        Console.WriteLine(penjumlahan.JumlahTigaAngka<int>(13, 02, 22));
+        
+
+    }
+}
