0978e52c3cd6ed19a18f503ac0a9f51e21a980df
PC13-IFLAB <PC13-IFLAB>
2023-03-20 08:05:51 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+
+class SimpleDataBase<T>
+{
+   private  List<T> storedData  = new List<T>();
+    private List<DateTime> inputDates = new List<DateTime>();
+    public SimpleDataBase() {
+        this.storedData = new List<T>() { };
+    }
+    public void AddNewData(T x)
+    {
+        this.storedData.Add(x);
+        this.inputDates.Add(DateTime.Now);
+    }
+    public void PrintAllData()
+    {
+        for (int i = 0;i<storedData.Count; i++)
+        {
+            Console.WriteLine("Data " + (i + 1) + 
+                " berisi: " + this.storedData[i] +
+                ", yang disimpan pada waktu UTC: " +
+                this.inputDates[i]);
+        }    
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        SimpleDataBase<int> simpleDataBase = new SimpleDataBase<int>();
+        simpleDataBase.AddNewData(13);
+        simpleDataBase.AddNewData(02);
+        simpleDataBase.AddNewData(21);
+        simpleDataBase.PrintAllData();
+    }
+
+}
